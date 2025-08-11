637ec669b6ac40fe3b8137efbb3232f1ea66d826
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-08-11 22:30:29 +0900

    fix

--- components/split-path.ts
+++ components/split-path.ts
@@ -6,16 +6,6 @@ export function splitPathDefinition(d: string): string[] {
     .map((s) => "M" + s);
 }
 
-function setAttributeIfNotNull(
-  element: SVGElement,
-  name: string,
-  value: string | null,
-) {
-  if (value !== null) {
-    element.setAttribute(name, value);
-  }
-}
-
 export function splitPath(path: SVGPathElement): SVGPathElement[] {
   // Split <path d="M ... M ..." /> into <path d="M ..." /> elements.
   const d = path.getAttribute("d");
@@ -23,19 +13,8 @@ export function splitPath(path: SVGPathElement): SVGPathElement[] {
     return [];
   }
   return splitPathDefinition(d).map((segment) => {
-    const newPath = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "path",
-    );
+    const newPath = path.cloneNode() as SVGPathElement;
     newPath.setAttribute("d", segment);
-    setAttributeIfNotNull(newPath, "stroke", path.getAttribute("stroke"));
-    setAttributeIfNotNull(
-      newPath,
-      "stroke-width",
-      path.getAttribute("stroke-width"),
-    );
-    setAttributeIfNotNull(newPath, "fill", path.getAttribute("fill"));
-    setAttributeIfNotNull(newPath, "fill-rule", path.getAttribute("fill-rule"));
     return newPath;
   });
 }
