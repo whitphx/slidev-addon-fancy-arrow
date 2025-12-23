de55b356ff1b31df99ee74d826a2fc98ba75e151
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-24 00:20:35 +0900

    Set 'display: contents' explicitly

--- components/FancyArrow.vue
+++ components/FancyArrow.vue
@@ -202,7 +202,15 @@ const { arrowSvg, textPosition } = useRoughArrow({
 </script>
 
 <template>
-  <div ref="root">
+  <div
+    ref="root"
+    style="display: contents"
+  >
+    <!--
+    "display: contents" ensures the root element doesn't affect the layout
+    so that the positions of the elements injected into the slots are not
+    affected by the root element.
+  -->
     <svg
       ref="svgContainer"
       :class="props.color ? `text-${props.color}` : ''"
