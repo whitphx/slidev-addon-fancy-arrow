c5eac7f51862a66fa7c12c9637879b5cd7300fbb
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-24 01:32:09 +0900

    fix

--- components/SlotAdapter.vue
+++ components/SlotAdapter.vue
@@ -5,6 +5,7 @@ import {
   getCurrentInstance,
   onMounted,
   isVNode,
+  computed,
   watchEffect,
   type RendererElement,
 } from "vue";
@@ -13,6 +14,10 @@ const emit = defineEmits<{
   firstChildElementMounted: [element: HTMLElement | null];
 }>();
 
+// Trick to get the refernce to the element injected to the slot.
+// Refs:
+// - https://github.com/orgs/vuejs/discussions/8936
+// - https://play.vuejs.org/#eNqNUk1vwjAM/StRLhQJFW3cGDBtiAOT9qHBMZeuNRCWJlXiMCTU/z4nXYFpbFqlqqnfe/aznQO/q6p054EP+QihrFSGMBGa0TOamrJqz4XcTa5G/fA5j1x/C/W/FKP+WSr6dbmVFTIH6AMsy8pYZIHMVtaUrJNGZfDRCeqGP+E9ji43eiXX6dYZTR4PoZLgObGlAvtcoTTaCT5kEQlYppT5eIgxtB56bTzfQP5+Ib51+xAT/MWCA7sDwY8YZnYN2MCzxRPs6XwES1N4Rew/wFdwRvngsaHde12Q7TNedDuPA5F6vXSzPYJ2bVPBaGDWkS84DSgM6rfWT3YH6SDqhK5piu1wLy3ZKYOs/++lHZiFVY9Rmam3FjTOtcNM59BjRj8arxEKVjdrjX4FvwnpaI0OWQGrzCtchJrjkCnRXqkuMRpcOh2TEfizQtIV+lgjSbrjCTUedEZBqsw6adWp829LC3Cb5hupCspBypre082qPwHA+gCI
 const instance = getCurrentInstance();
 const firstChildElementRef = ref<RendererElement>();
 onMounted(() => {
@@ -44,15 +49,12 @@ onMounted(() => {
   }
 });
 
-const shouldWrap = ref(false);
-watchEffect(() => {
+const shouldWrap = computed(() => {
   if (firstChildElementRef.value == null) {
-    return;
+    return false;
   }
 
-  if (!(firstChildElementRef.value instanceof HTMLElement)) {
-    shouldWrap.value = true;
-  }
+  return !(firstChildElementRef.value instanceof HTMLElement);
 });
 
 const wrapperRef = useTemplateRef("wrapper");
